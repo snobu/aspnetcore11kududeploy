915219891e03f1fa242708909c18d8cc3190ee3f
snobu <foo@snobu.org>
2017-03-08 16:31:34 +0200

    Added logging

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,19 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
+using Microsoft.ApplicationInsights;
+using Microsoft.Extensions.Configuration;
 
 namespace aspnetcorekeyvault.Controllers
 {
-    public class HomeController : Controller
+     public class HomeController : Controller
     {
+        readonly IConfiguration _config;
+        readonly IOptions<MyConfig> _myConfig;
+        readonly ILogger _logger;
+
+        public HomeController(IOptions<MyConfig> myConfig, IConfiguration config, ILoggerFactory loggerFactory)
+        {
+            _config = config;
+            _myConfig = myConfig;
+            _logger = loggerFactory.CreateLogger<HomeController>();
+        }
+
         public IActionResult Index()
         {
+            _logger.LogInformation("Some information log");
+            _logger.LogWarning("This is a warning");
+            _logger.LogError("This is an error");
+
             return View();
         }
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            var telemetry = new TelemetryClient();
+            telemetry.TrackEvent("About");
+
+            ViewData["Message"] = $"App name is '{_myConfig.Value.AppName}', Secret is '{_myConfig.Value.BigSecret}', Db='{_config.GetConnectionString("MyDatabase")}'";
 
             return View();
         }
@@ -24,7 +46,9 @@ namespace aspnetcorekeyvault.Controllers
         {
             ViewData["Message"] = "Your contact page.";
 
-            return View();
+            throw new Exception("Contact is busted :(");
+
+            //return View();
         }
 
         public IActionResult Error()
